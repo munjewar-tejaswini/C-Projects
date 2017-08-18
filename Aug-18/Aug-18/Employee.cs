47c175f4ec4405c1277fde1d3eeb13a96509961f
Tejaswini Munjewar <Tejaswini.Munjewar@xoriant.com>
2017-08-18 11:45:15 +0530

    C#-Aug-18

---
+++ Aug-18/Aug-18/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aug_18
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public int Salary { get; set; }
+        public Employee(int id, string name, int salary)
+        {
+            ID = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("ID : {0} \nName : {1} \nSalary : {2}\n", ID,Name,Salary);
+        }
+    }
+}
--- Aug-18/Aug-18/Program.cs
+++ Aug-18/Aug-18/Program.cs
@@ -10,6 +10,17 @@ namespace Aug_18
     {
         static void Main(string[] args)
         {
+            List<Employee> employee = new List<Employee>();
+            employee.Add(new Employee(101, "Tejaswini", 30000));
+            employee.Add(new Employee(102, "Ashwini", 30000));
+            employee.Add(new Employee(103, "Minal", 30000));
+            employee.Add(new Employee(104, "Pooja", 30000));
+
+            foreach (var emp in employee)
+            {
+                Console.WriteLine(emp);
+            }
+            Console.ReadLine();
         }
     }
 }
